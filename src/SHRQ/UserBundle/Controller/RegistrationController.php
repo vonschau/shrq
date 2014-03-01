0cda402652c7b6a6fa9b2c706c653b2aafaf4250
Martin Cajthaml <martin.cajthaml@symbio.cz>
2014-03-01 20:02:53 +0100

    oprava

--- src/SHRQ/UserBundle/Controller/RegistrationController.php
+++ src/SHRQ/UserBundle/Controller/RegistrationController.php
@@ -4,7 +4,11 @@ namespace SHRQ\UserBundle\Controller;
 
 use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 use Symfony\Component\Security\Core\Exception\AccessDeniedException;
+use Symfony\Component\Security\Core\Exception\AccountStatusException;
 use FOS\UserBundle\Model\UserInterface;
 use PayPal\Api\Amount;
 use PayPal\Api\Details;
@@ -19,9 +23,9 @@ class RegistrationController extends Controller
 
     public function registerAction()
     {
-        $form = $this->container->get('fos_user.registration.form');
-        $formHandler = $this->container->get('fos_user.registration.form.handler');
-        $confirmationEnabled = $this->container->getParameter('fos_user.registration.confirmation.enabled');
+        $form = $this->get('fos_user.registration.form');
+        $formHandler = $this->get('fos_user.registration.form.handler');
+        $confirmationEnabled = $this->getParameter('fos_user.registration.confirmation.enabled');
 
         $process = $formHandler->process($confirmationEnabled);
         if ($process) {
@@ -29,7 +33,7 @@ class RegistrationController extends Controller
 
             $authUser = false;
             if ($confirmationEnabled) {
-                $this->container->get('session')->set('fos_user_send_confirmation_email/email', $user->getEmail());
+                $this->get('session')->set('fos_user_send_confirmation_email/email', $user->getEmail());
                 $route = 'fos_user_registration_check_email';
             } else {
                 $authUser = true;
@@ -37,7 +41,7 @@ class RegistrationController extends Controller
             }
 
             $this->setFlash('fos_user_success', 'registration.flash.user_created');
-            $url = $this->container->get('router')->generate($route);
+            $url = $this->get('router')->generate($route);
             $response = new RedirectResponse($url);
 
             if ($authUser) {
@@ -47,7 +51,7 @@ class RegistrationController extends Controller
             return $response;
         }
 
-        return $this->container->get('templating')->renderResponse('FOSUserBundle:Registration:register.html.'.$this->getEngine(), array(
+        return $this->get('templating')->renderResponse('FOSUserBundle:Registration:register.html.'.$this->getEngine(), array(
             'form' => $form->createView(),
         ));
     }
@@ -57,15 +61,15 @@ class RegistrationController extends Controller
      */
     public function checkEmailAction()
     {
-        $email = $this->container->get('session')->get('fos_user_send_confirmation_email/email');
-        $this->container->get('session')->remove('fos_user_send_confirmation_email/email');
-        $user = $this->container->get('fos_user.user_manager')->findUserByEmail($email);
+        $email = $this->get('session')->get('fos_user_send_confirmation_email/email');
+        $this->get('session')->remove('fos_user_send_confirmation_email/email');
+        $user = $this->get('fos_user.user_manager')->findUserByEmail($email);
 
         if (null === $user) {
             throw new NotFoundHttpException(sprintf('The user with email "%s" does not exist', $email));
         }
 
-        return $this->container->get('templating')->renderResponse('FOSUserBundle:Registration:checkEmail.html.'.$this->getEngine(), array(
+        return $this->get('templating')->renderResponse('FOSUserBundle:Registration:checkEmail.html.'.$this->getEngine(), array(
             'user' => $user,
         ));
     }
@@ -75,7 +79,7 @@ class RegistrationController extends Controller
      */
     public function confirmAction($token)
     {
-        $user = $this->container->get('fos_user.user_manager')->findUserByConfirmationToken($token);
+        $user = $this->get('fos_user.user_manager')->findUserByConfirmationToken($token);
 
         if (null === $user) {
             throw new NotFoundHttpException(sprintf('The user with confirmation token "%s" does not exist', $token));
@@ -85,8 +89,8 @@ class RegistrationController extends Controller
         $user->setEnabled(true);
         $user->setLastLogin(new \DateTime());
 
-        $this->container->get('fos_user.user_manager')->updateUser($user);
-        $response = new RedirectResponse($this->container->get('router')->generate('fos_user_registration_confirmed'));
+        $this->get('fos_user.user_manager')->updateUser($user);
+        $response = new RedirectResponse($this->get('router')->generate('fos_user_registration_confirmed'));
         $this->authenticateUser($user, $response);
 
         return $response;
@@ -97,7 +101,7 @@ class RegistrationController extends Controller
      */
     public function confirmedAction()
     {
-        $user = $this->container->get('security.context')->getToken()->getUser();
+        $user = $this->get('security.context')->getToken()->getUser();
         if (!is_object($user) || !$user instanceof UserInterface) {
             throw new AccessDeniedException('This user does not have access to this section.');
         }
@@ -108,7 +112,7 @@ class RegistrationController extends Controller
         $price = $prices[$user->getTicketType()];
 
         if ($user->getPaymentType() === 1) {
-            $paypal = $this->container->get('paypal');
+            $paypal = $this->get('paypal');
 
             $payer = new Payer();
             $payer->setPaymentMethod('paypal');
@@ -152,7 +156,7 @@ class RegistrationController extends Controller
             }
         }
 
-        return $this->container->get('templating')->renderResponse('FOSUserBundle:Registration:confirmed.html.'.$this->getEngine(), array(
+        return $this->get('templating')->renderResponse('FOSUserBundle:Registration:confirmed.html.'.$this->getEngine(), array(
             'user' => $user,
             'payment_types' => $payment_types,
             'ticket_types' => $ticket_types,
@@ -170,8 +174,8 @@ class RegistrationController extends Controller
     protected function authenticateUser(UserInterface $user, Response $response)
     {
         try {
-            $this->container->get('fos_user.security.login_manager')->loginUser(
-                $this->container->getParameter('fos_user.firewall_name'),
+            $this->get('fos_user.security.login_manager')->loginUser(
+                $this->getParameter('fos_user.firewall_name'),
                 $user,
                 $response);
         } catch (AccountStatusException $ex) {
@@ -186,11 +190,11 @@ class RegistrationController extends Controller
      */
     protected function setFlash($action, $value)
     {
-        $this->container->get('session')->getFlashBag()->set($action, $value);
+        $this->get('session')->getFlashBag()->set($action, $value);
     }
 
     protected function getEngine()
     {
-        return $this->container->getParameter('fos_user.template.engine');
+        return $this->getParameter('fos_user.template.engine');
     }
 }
