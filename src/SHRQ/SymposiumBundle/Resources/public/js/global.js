dbb832fd4982fbcd36cda2ab386bec505abf90e8
Martin Cajthaml <martin.cajthaml@symbio.cz>
2014-03-01 19:32:46 +0100

    oprava

--- src/SHRQ/SymposiumBundle/Resources/public/js/global.js
+++ src/SHRQ/SymposiumBundle/Resources/public/js/global.js
@@ -6,47 +6,71 @@ $(function () {
 
 			if(!$(this).hasClass('link')) {
 
-		        $(this).parent('li').siblings().find('.opened').removeClass('opened');
-		        $(this).addClass('opened').siblings('ul, div, form').addClass('opened');
+				$(this).parent('li').siblings().find('.opened').removeClass('opened');
+				$(this).addClass('opened').siblings('ul, div, form').addClass('opened');
 
-	        e.preventDefault();
+			e.preventDefault();
 
 		}
 
 	});
 
-    $('table a').on('click', function(e) {
+	$('table a').on('click', function(e) {
 
-    	var that = $(this);
+		var that = $(this);
 
-    	if(!$(this).hasClass('no-detail')) {
+		if(!$(this).hasClass('no-detail')) {
 
-	    	$('table').find('.opened-td').removeClass('opened-td');
-	    	$(this).closest('td').addClass('opened-td');
+			$('table').find('.opened-td').removeClass('opened-td');
+			$(this).closest('td').addClass('opened-td');
 
-	    	var row = $(this).closest('tr').nextAll('.opener').first().find('td');
+			var row = $(this).closest('tr').nextAll('.opener').first().find('td');
 
-	    	menu.appendTo(row);
+			menu.appendTo(row);
 
-	    	var target = $($(this).attr('href'));
+			var target = $($(this).attr('href'));
 
 
-	    	$('#menu').find('.opened').removeClass('opened');
+			$('#menu').find('.opened').removeClass('opened');
 
-	    	target
-	    		.addClass('opened')
-	    		.siblings('a').addClass('opened')
-	    		.closest('.person-detail').addClass('opened')
-	    		.siblings('a').addClass('opened')
-	    		.closest('.sub-1').addClass('opened')
-	    		.siblings('a').addClass('opened');
+			target
+				.addClass('opened')
+				.siblings('a').addClass('opened')
+				.closest('.person-detail').addClass('opened')
+				.siblings('a').addClass('opened')
+				.closest('.sub-1').addClass('opened')
+				.siblings('a').addClass('opened');
 
 			$('html, body').animate({scrollTop: that.offset().top}, 0);
 
-    	}
+		}
 
-    	e.preventDefault();
+		e.preventDefault();
 
-    });
+	});
+
+   	$('.menu-link').on('click', function() {
+		$('table').find('.opened-td').removeClass('opened-td');
+
+		var row = $('table tr.opener:first').find('td');
+
+		menu.appendTo(row);
+
+		var target = $($(this).attr('href'));
+
+		$('#menu').find('.opened').removeClass('opened');
+
+		target
+			.addClass('opened')
+			.siblings('form').addClass('opened')
+			.siblings('a').addClass('opened')
+			.closest('.person-detail').addClass('opened')
+			.siblings('a').addClass('opened')
+			.closest('.sub-1').addClass('opened')
+			.siblings('a').addClass('opened');
+
+		$('html, body').animate({scrollTop: menu.offset().top}, 500);
+	});
+
 
 });
